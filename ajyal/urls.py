5ef81903cdc603ae806ebca1fbc4ed3354a8087e
Zouhair guertaoui <zouhairzg1@gmail.com>
2024-03-06 19:19:33 +0100

    fiveth commit

--- ajyal/urls.py
+++ ajyal/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='user-logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html',form_class=user_forms.UserSetPasswordForm), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),
     path('store_reading_time/<int:pk>/',views.store_reading_time,name='time_reading'),
 ]
